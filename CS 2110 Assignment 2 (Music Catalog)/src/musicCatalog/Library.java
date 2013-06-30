b959237a0a91e6882da2a77fbeb9926a86b8560b
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-06-30 19:14:09 -0400

    Added more methods that may be useful in the program

--- CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Library.java
+++ CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Library.java
@@ -3,8 +3,8 @@ package musicCatalog;
 public class Library {
 	
 	//Declare Variables To Hold Information About the Library 
-	private int numberOfSongs;
-	private int numberOfGenres;
+	private static int numberOfSongs = 0;
+	private static int numberOfGenres = 0;
 	private String libraryName;
 	
 	//Accessory Methods
@@ -29,6 +29,33 @@ public class Library {
 		libraryName = name;
 	}
 	
+	//For working on existing catalog
+	public void importLibrary(String filepath) {
+		
+	}
 	
+	//For saving out catalog
+	public void exportLibrary() {
+		
+	}
 	
+	//To add a song to the catalog
+	public void addSongs() {
+		Catalog.out.println("What is the name of the Artist or Composer?");
+	}
+	
+	//To remove a song from the catalog
+	public void deleteSongs() {
+		
+	}
+	
+	//To check the information entered by the user
+	public void spellChecker() {
+		
+	}
+	
+	//Constructor Method
+	public Library() {
+		
+	}
 }//Close Library Class
