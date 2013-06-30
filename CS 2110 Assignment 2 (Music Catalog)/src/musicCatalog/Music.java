d5aeccd99abad356e2f1b8ab2791dba2bcbf7f1c
Omer <oas22@cornell.edu>
2013-06-29 20:49:37 -0400

    "Created Accessor Methods for Music Class"

--- CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Music.java
+++ CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Music.java
@@ -3,9 +3,27 @@ package musicCatalog;
 public class Music {
 	
 	private int songId;	
-	private String title, genre, comment;
+	private String title;
+	private String genre;
+	private String comment;
 	private double duration;
 	
+	//Accessor Comments
+	public int getId(){
+		return songId;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public String getGenre(){
+		return genre;
+	}
+	public String getComment(){
+		return comment;
+	}
+	public double getDuration(){
+		return duration;
+	}
 	
 	
 
