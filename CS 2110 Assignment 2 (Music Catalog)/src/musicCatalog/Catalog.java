d393ba99c0dc7564cb59483f66b631c190d4116c
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-06-30 19:13:20 -0400

    Added Import and initialization of Screen IO for use in other classes Use it by using either Catalog.in and Catalog.out

--- CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Catalog.java
+++ CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Catalog.java
@@ -1,8 +1,19 @@
 package musicCatalog;
 
-public class Catalog {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
+public class Catalog {
+	
+	//Create IO objects that can be used within other classes
+	static InputStreamReader isr = new InputStreamReader(System.in);
+	static BufferedReader in = new BufferedReader(isr);
+	static PrintWriter out = new PrintWriter(System.out,true);
+	
 	public static void main(String[] args) {
+
+		
 		String test="Another test";
 		String test2="Hello Ish";
 
