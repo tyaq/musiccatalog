ecde355f5e240e7ca4be916f6ca84cc9c78019c9
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-06-29 20:47:58 -0400

    Created Class Variables for Library Class, and the Accessory Methods for them.

--- CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Library.java
+++ CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Library.java
@@ -1,5 +1,24 @@
 package musicCatalog;
 
 public class Library {
- String yea = "yea";
-}
+	
+	//Declare Variables To Hold Information About the Library 
+	private int numberOfSongs;
+	private int numberOfGenres;
+	private String libraryName;
+	
+	//Accessory Methods
+	public int getNumberOfSongs() { //For numberOfSongs
+		return numberOfSongs;
+	}
+	public int getNumberOfGenres() {
+		return numberOfGenres;
+	}
+	public String getLibraryNames() {
+		return libraryName;
+	}
+	
+	
+	
+	
+}//Close Library Class
