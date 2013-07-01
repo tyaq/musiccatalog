602cb5aa6b2fe5dae111284d2df1c2307951315c
Omer <oas22@cornell.edu>
2013-07-01 18:22:14 -0400

    "added the orchestral class"

---
+++ CS 2110 Assignment 2 (Music Catalog)/src/musicCatalog/Orchestral.java
@@ -0,0 +1,40 @@
+package musicCatalog;
+
+
+
+public class Orchestral extends Music {
+
+	private String composer;
+	private String[] instruments;
+	private final String genre="orchestra";
+	//Accessory Methods
+	
+	public String getComposer(){
+		return composer;
+	}
+	public String[] getInstruments(){
+		return instruments;
+	}
+	
+	
+	//Mutator Methods
+	
+	public void setComposer(String composer){
+		this.composer=composer;
+	}
+	public void setInstruments(String[] instruments){
+		this.instruments=instruments;
+	}
+
+	//Methods to do stuff
+	public String toString(){// display all music data as a string
+		return (getComposer()+"\n\t"+getTitle()+"\n\t"+getGenre()+
+				"\n\tlength= "+getDuration()+"\n\tInstruments include: "+
+				getInstruments()+"\n\tComposed in"+getYear()+"\n\t"+
+				getComment()+"\n\t"+getSongId());
+		
+
+		
+	}
+
+}
